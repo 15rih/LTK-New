3314976403e00754604753283136ae4ac78b1775
Fypher <129924494+15rih@users.noreply.github.com>
2025-01-01 16:41:35 -0800

    Create blacklist.lua

---
+++ blacklist.lua
@@ -0,0 +1,5 @@
+if game.Players.LocalPlayer.UserId == () then
+  game.Players.LocalPlayer:Kick("Blacklisted from LTK Hub | Reason: nil")
+elseif game.Players.LocalPlayer.UserId == () then
+  game.Players.LocalPlayer:Kick("Blacklisted from LTK Hub | Reason: nil")
+end
