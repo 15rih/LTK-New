14221695300d37b0e7eaf0e904c3d316884d605d
Fypher <129924494+15rih@users.noreply.github.com>
2025-01-31 14:56:48 -0800

    Update LTK-Free.lua

--- LTK-Scripts/LTK-Free.lua
+++ LTK-Scripts/LTK-Free.lua
@@ -1,3 +1,8 @@
 if game.PlaceId == (109109677622036) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/15rih/LTK-New/refs/heads/main/LTK-Scripts/ChicagoTrenches.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/15rih/LTK-New/refs/heads/main/LTK-Scripts/ChicagoTrenches.lua"))()
+elseif game.PlaceId == (2512643572) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/15rih/LTK-New/refs/heads/main/LTK-Scripts/BGS.lua"))()
+else
+    game.Players.LocalPlayer:Kick("LTK Hub [Free] | Unsupported Game, If this is an error Contact a Creator | Discord was copied to clipboard")
+    setclipboard("https://discord.gg/dfzHWSGdPc")
 end
