052a4a3b423bf0e8e99d45c3eba31bcbe7918284
Fypher <129924494+15rih@users.noreply.github.com>
2025-01-31 01:23:38 -0800

    Update LTK-Free.lua

--- LTK-Scripts/LTK-Free.lua
+++ LTK-Scripts/LTK-Free.lua
@@ -1,3 +1,3 @@
-if game.PlaceId == (93731064052368) then
+if game.PlaceId == (109109677622036) then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/15rih/LTK-New/refs/heads/main/LTK-Scripts/ChicagoTrenches.lua"))()
 end
