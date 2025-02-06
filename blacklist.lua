480bcdd9141d2467963cdf795c975561a05f3ff2
Fypher <129924494+15rih@users.noreply.github.com>
2025-02-06 12:59:36 -0800

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,5 +1,5 @@
 if game.Players.LocalPlayer.UserId == (4571658807) then
   game.Players.LocalPlayer:Kick("Blacklisted from LTK Hub | Reason: Refunding Payment")
-elseif game.Players.LocalPlayer.UserId == (22) then
+elseif game.Players.LocalPlayer.UserId == (6154346059) then
   game.Players.LocalPlayer:Kick("Blacklisted from LTK Hub | Reason: nil")
 end
