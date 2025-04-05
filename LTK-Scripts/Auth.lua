5b75a788ae6d325ce189ea094eb88548229a5be9
Fypher <129924494+15rih@users.noreply.github.com>
2025-04-04 23:57:09 -0700

    dddd

--- LTK-Scripts/Auth.lua
+++
@@ -1,2 +0,0 @@
--- idk why tf u here nga but ig u tryn skid :cc
-loadstring(game:HttpGet("https://raw.githubusercontent.com/15rih/LTK-New/refs/heads/main/LTK-Scripts/dkdfkhn234j3hk34hkkhkfdh.lua"))()
