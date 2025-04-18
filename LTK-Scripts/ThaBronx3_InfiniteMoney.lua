e89f895632d7548de1e59e7ae6e45ac02d98ce70
Fypher <129924494+15rih@users.noreply.github.com>
2025-04-17 22:08:04 -0700

    Create ThaBronx3_InfiniteMoney.lua

---
+++ LTK-Scripts/ThaBronx3_InfiniteMoney.lua
@@ -0,0 +1,37 @@
+-- FUCK ALL DA SKIDS YUHHH WE ON YO AZ | FUCK ALL DA SKIDS YUHHH WE ON YO AZ
+-- FUCK ALL DA SKIDS YUHHH WE ON YO AZ | FUCK ALL DA SKIDS YUHHH WE ON YO AZ
+-- FUCK ALL DA SKIDS YUHHH WE ON YO AZ | FUCK ALL DA SKIDS YUHHH WE ON YO AZ
+-- FUCK ALL DA SKIDS YUHHH WE ON YO AZ | FUCK ALL DA SKIDS YUHHH WE ON YO AZ
+-- FUCK ALL DA SKIDS YUHHH WE ON YO AZ | FUCK ALL DA SKIDS YUHHH WE ON YO AZ
+-- FUCK ALL DA SKIDS YUHHH WE ON YO AZ | FUCK ALL DA SKIDS YUHHH WE ON YO AZ
+-- FUCK ALL DA SKIDS YUHHH WE ON YO AZ | FUCK ALL DA SKIDS YUHHH WE ON YO AZ
+-- FUCK ALL DA SKIDS YUHHH WE ON YO AZ | FUCK ALL DA SKIDS YUHHH WE ON YO AZ
+
+
+-- DMCA GON HURT LIKE A BIH
+-- COVERED BY: [ GNU AFFERO GENERAL PUBLIC LICENSE v3 ] - do not ignore
+
+
+
+
+
+
+
+for i=1,999 do
+	fireproximityprompt(workspace["IceFruit Sell"].ProximityPrompt)
+end
+
+
+task.wait(4.5) do
+	if LocalPlayer.stored.FilthyStack.Value == LocalPlayer:GetAttribute("MaxMoney") then
+			CoreGui.RobloxGui.Backpack.Visible =           true
+			LocalPlayer.PlayerGui.Hunger.Enabled =         true
+			LocalPlayer.PlayerGui.HealthGui.Enabled =      true
+			LocalPlayer.PlayerGui.Run.Enabled =            true
+			LocalPlayer.PlayerGui.SleepGui.Enabled =       true
+			LocalPlayer.PlayerGui.MoneyGui.Enabled =       true
+			LocalPlayer.PlayerGui.NewMoneyGui.Enabled =    true
+			LocalPlayer.Character.HumanoidRootPart.CFrame = oldPos
+			Notifications:Notify("[ LTK: Hub ] Money Dupe has been completed, you can now go wash your money & repeat.", 10, "success")
+    end
+end
