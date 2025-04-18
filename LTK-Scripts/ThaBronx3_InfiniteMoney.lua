703348c7b9efb6f37b4023a69a02c341ca62721e
Fypher <129924494+15rih@users.noreply.github.com>
2025-04-17 22:15:17 -0700

    Update ThaBronx3_InfiniteMoney.lua

--- LTK-Scripts/ThaBronx3_InfiniteMoney.lua
+++ LTK-Scripts/ThaBronx3_InfiniteMoney.lua
@@ -12,6 +12,13 @@
 -- COVERED BY: [ GNU AFFERO GENERAL PUBLIC LICENSE v3 ] - do not ignore
 
 
+--[[
+⚠️ WARNING: Unauthorized use of this code will not be accepted.
+This code is licensed under the GNU Affero General Public License V3. 
+The AGPL mandates that any modified versions of the code, when shared, must also be licensed under the AGPL and include the source code for any changes made. 
+Be aware that DMCA claims will be filed if you do not comply with the license!
+]]
+
 
 
 
