75cbedfa614653c450b4c9543749b0f40a6c643b
Fypher <129924494+15rih@users.noreply.github.com>
2025-06-10 21:05:39 -0700

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -282,17 +282,9 @@ end
         UI["2"] = InstanceNew("Frame", UI["1"])
         UI["2"]["BorderSizePixel"] = 0
         UI["2"]["BackgroundColor3"] = Color3FromRGB(24, 24, 24)
-        local UserInputService = game:GetService("UserInputService")
-		      if UserInputService.KeyboardEnabled then
-							UI["2"]["Size"] = UDim2New(0, 362, 0, 259)	
-			   --UI["2"]["Size"] = UDim2New(0, 562, 0, 459)
-		print('keyboard')
-		else
-			UI["2"]["Size"] = UDim2New(0, 462, 0, 459)
-			print("not an available keyboard")
-		end
-        --UI["2"]["Size"] = UDim2New(0, 562, 0, 459)
-        UI["2"]["Position"] = UDim2New(0, 527, 0, 168)
+        UI["2"]["Size"] = UDim2New(0, 562, 0, 459)
+	UI["2"]["Position"] = UDim2New(0.5, 0, 0.5, 168)				
+        --UI["2"]["Position"] = UDim2New(0, 527, 0, 168)
         UI["2"]["BorderColor3"] = Color3FromRGB(0, 0, 0)
         UI["2"]["Name"] = [[BackgroundFrame]]
         
